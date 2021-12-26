d091e2351171de45f7d6fc72551deddb2a6c92d2
Dawntraoz <albasilventefu@gmail.com>
2021-12-26 13:34:26 +0100

    feat: dynamic components coming from ButterCMS initial definition

--- app.vue
+++ app.vue
@@ -1,12 +1,37 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <component
+    :is="component.type"
+    :fields="component.fields"
+    v-for="component in story.fields.body"
+  />
 </template>
 
 <script setup>
 const { $butter } = useNuxtApp();
 
 const { data: { data: story } } = await $butter.page.retrieve('*','landing-page-with-components');
-console.log(story);
 </script>
+
+<style>
+body {
+  @apply bg-cool-gray-200;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  @apply font-bold pb-6;
+}
+
+h1 { @apply text-3xl md:text-5xl md:leading-tight; }
+h2 { @apply text-xl md:text-2xl; }
+h3 { @apply text-lg md:text-xl; }
+h4,
+h5,
+h6 {
+  @apply text-md md:text-lg;
+}
+</style>
--- plugins/buttercms.js
+++ plugins/buttercms.js
@@ -1,9 +1,20 @@
 import { defineNuxtPlugin } from '#app';
 import Butter from 'buttercms';
 
-export default defineNuxtPlugin(() => {
+// ButterCMS field dynamic components
+import Hero from '~/components/Hero.vue';
+import Features from '~/components/Features.vue';
+import Testimonials from '~/components/Testimonials.vue';
+import TwoColumnWithImage from '~/components/TwoColumnWithImage.vue';
+
+export default defineNuxtPlugin((nuxtApp) => {
   const config = useRuntimeConfig();
-  
+
+  nuxtApp.vueApp.component('hero', Hero);
+  nuxtApp.vueApp.component('features', Features);
+  nuxtApp.vueApp.component('testimonials', Testimonials);
+  nuxtApp.vueApp.component('two-column-with-image', TwoColumnWithImage);
+
   return {
     provide: {
       butter: Butter(config.API_TOKEN),
