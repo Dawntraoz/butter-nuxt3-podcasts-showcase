e4cefc93a87b380bf4725536c7c11baeefcafd65
Dawntraoz <albasilventefu@gmail.com>
2022-01-09 22:22:24 +0100

    feat: new icon, private env variable and favicon

--- components/PodcastEpisodesList.vue
+++ components/PodcastEpisodesList.vue
@@ -18,9 +18,14 @@
           loading="lazy"
         />
       </figure>
-      <h3 class="col-start-2 col-end-5 pt-3 pb-2">
+      <h3 class="col-start-2 col-end-5 pb-2">
         <a :href="episode.url" target="_blank" rel="noopener noreferrer" class="text-indigo-800">
-          {{ episode.title }} ↗
+          <svg class="w-6 h-6" fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M9.25 4.75H6.75C5.64543 4.75 4.75 5.64543 4.75 6.75V17.25C4.75 18.3546 5.64543 19.25 6.75 19.25H17.25C18.3546 19.25 19.25 18.3546 19.25 17.25V14.75"></path>
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M19.25 9.25V4.75H14.75"></path>
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M19 5L11.75 12.25"></path>
+          </svg>
+          {{ episode.title }}
         </a>
       </h3>
       <p class="col-span-full text-sm line-clamp-2">{{ episode.description }}</p>
